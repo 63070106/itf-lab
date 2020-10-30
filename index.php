49fda268f1b67337f36c403c7e343e6f3f4ac7ea
63070106 <73683220+63070106@users.noreply.github.com>
2020-10-30 12:49:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "นายปัณณธร กระแสร์วิก (ปัณณ์)<br>";
-?>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <h1>IT@KMITL</h1>
+        <p>ปัณณธร กระแสร์วิก (ปัณณ์)</p>
+        <img src="cat.jpg">
+        <img src="dog.jpg">
+        <br>
+        <a href="http://www.it.kmitl.ac.th">IT KMITL</a>
+        <a href="https://onlearn.it.kmitl.ac.th/">ONLEARN</a>
+                
+    </body>
+</html>
