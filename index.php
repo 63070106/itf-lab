7b17f103770397ea60f997796afd77594044c53c
63070106 <73683220+63070106@users.noreply.github.com>
2020-10-30 12:31:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 echo "<h2>IT@KMITL</h2>";
 echo "นายปัณณธร กระแสร์วิก (ปัณณ์)<br>";
-echo "<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fcathousethailand%2F&psig=AOvVaw3nPfcNXhPwKUUqcMJNR2FL&ust=1604118993415000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJirsZG_2-wCFQAAAAAdAAAAABAD">";
 ?>
